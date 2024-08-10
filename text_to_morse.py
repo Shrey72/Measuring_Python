c919aff291d3be498fa42261c5d6b6febe8f74e4
Shrey Mishra <mishrashrey72@gmail.com>
2024-08-11 00:10:23 +0530

    text to morse

---
+++ text_to_morse.py
@@ -0,0 +1,22 @@
+morse_code_dict = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+    'Z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-', 
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', 
+    '0': '-----', ' ': '/'
+}
+
+def text_to_morse(text):
+    morse_code=""
+    for letter in Text:
+        if letter in morse_code_dict:
+            morse_code+=morse_code_dict[letter]+" "
+        else:
+            morse_code+='?'
+    print (morse_code.strip())
+
+Text=input("Enter the text: ").upper()
+text_to_morse(Text)
